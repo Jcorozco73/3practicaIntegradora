165bdff57e2249ee37a66815216f590803a4d4cc
Jcorozco73 <jc.martin.orozco@gmail.com>
2024-01-17 09:27:07 -0300

    proyecto final update

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import cartRouter from "./router/cart.router.js";
 import restoreRouter from './router/restorePass.router.js'
 
 const app = Express()
-const PORT = 8080
+const PORT = process.env.PORT||8080
 
 
 const SECRET = config.secret 
